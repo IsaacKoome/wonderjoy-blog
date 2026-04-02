7c9b7d310aceaddf28bb58e47705e18554fc0504
Isaac Koome <koomeisaac16@gmail.com>
2026-04-02 08:06:50 +0300

    Google analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Link from 'next/link';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -86,6 +87,9 @@ export default function RootLayout({
             </div>
           </div>
         </footer>
+        
+        {/* Google Analytics */}
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
       </body>
     </html>
   );
