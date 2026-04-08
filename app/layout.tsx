c77b44eb9689a52647dfd58bc7bf97f7cd7956e0
Isaac Koome <koomeisaac16@gmail.com>
2026-04-08 06:11:16 +0300

    I added Google adsense script in main branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1335773994240018"
+          crossOrigin="anonymous"
+        ></script>
         <meta name="p:domain_verify" content="c13622a3100474f214fd617ea3edad41"/>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/favicon.ico" />
