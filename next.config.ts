9ce981c44ce4212f70253b41b29c3dd712e6a3f9
Isaac Koome <koomeisaac16@gmail.com>
2026-04-14 17:13:41 +0300

    Trying to fix google not indexig my pages error on next config js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.wonderjoyai.com",
+          },
+        ],
+        destination: "https://wonderjoyai.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
